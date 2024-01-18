d93b89f9e36eeca3e66d156bef5671e9c6f8b0a7
Jordi A <rauxasoft@gmail.com>
2024-01-18 09:33:35 +0100

    commit inicial

---
+++ src/main/java/com/sinensia/pollosfelices/PollosfelicesApplication.java
@@ -0,0 +1,13 @@
+package com.sinensia.pollosfelices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollosfelicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollosfelicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sinensia/pollosfelices/PollosfelicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinensia.pollosfelices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollosfelicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
