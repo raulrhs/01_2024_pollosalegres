6f6b72a0ec9d96d57ba788c886a7b946d3ef1bb0
Jordi A <rauxasoft@gmail.com>
2024-01-30 11:51:06 +0100

    se crea clase ProductoController

---
+++ src/main/java/com/sinensia/pollosfelices/backend/presentation/controllers/ProductoController.java
@@ -0,0 +1,10 @@
+package com.sinensia.pollosfelices.backend.presentation.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/productos")
+public class ProductoController {
+
+}
