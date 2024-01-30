cea4ff9346bdab7332ce0047cadc02079f6bcda1
Jordi A <rauxasoft@gmail.com>
2024-01-30 11:55:33 +0100

    iniciamos imnplementacion de ProductoController

--- src/main/java/com/sinensia/pollosfelices/backend/presentation/controllers/ProductoController.java
+++ src/main/java/com/sinensia/pollosfelices/backend/presentation/controllers/ProductoController.java
@@ -3,8 +3,15 @@ package com.sinensia.pollosfelices.backend.presentation.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sinensia.pollosfelices.backend.business.services.ProductoServices;
+
 @RestController
 @RequestMapping("/productos")
 public class ProductoController {
 
+	private ProductoServices productoServices;
+	
+	public ProductoController(ProductoServices productoServices) {
+		this.productoServices = productoServices;
+	}
 }
